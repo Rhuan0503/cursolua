500bc06552ef4310af3df22bfe910f37803bae1a
Rhuan0503 <rhuansousa050312@gmail.com>
2026-04-02 12:50:57 -0300

    exercicio3

---
+++ exe01/exe03.lua
@@ -0,0 +1,15 @@
+print("primeira nota")
+local n1 = io.read("n")
+
+print("segunda nota")
+local n2 = io.read("n")
+
+print("terceira nota")
+local n3 = io.read("n")
+
+print("quarta nota")
+local n4 = io.read("n")
+
+local media = (n1+n2+n3+n4) /4
+
+print("a media do aluno e ".. media )
